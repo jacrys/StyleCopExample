4db361fdfa7c12c73512666e1c50b7cf0657b3a4
Keith Lewis (Jacrys) <kclewis0614@gmail.com>
2022-04-14 10:41:49 -0500

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StyleCopExample.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
